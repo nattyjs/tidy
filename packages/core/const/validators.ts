a179ee39563de625f1ee3565a79215a34b3e6f89
Ajay Ojha <ojhaajay@outlook.com>
2024-05-04 20:56:18 +0530

    chore(core): update type definition

--- packages/core/const/validators.ts
+++ packages/core/const/validators.ts
@@ -1,5 +1,5 @@
 
-import { AlphaConfig, ArrayConfig,DefaultConfig, ControlValidators, RequiredConfig, ValidatorFn, CreditCardConfig, DifferentConfig, DigitConfig, EmailConfig, ExtensionConfig, FactorConfig, RelationalOperatorConfig, HexColorConfig, IpConfig, MessageConfig, MaxDateConfig, NumberConfig, MaxTimeConfig, MinDateConfig, MinTimeConfig, NumericConfig, PasswordConfig, PatternConfig, RangeConfig, TimeConfig, BaseValidatorConfig } from '@nattyjs/validation-decorators'
+import { AlphaConfig, ArrayConfig,DefaultConfig, ControlValidators, RequiredConfig, ValidatorFn, CreditCardConfig, DifferentConfig, DigitConfig, EmailConfig, ExtensionConfig, FactorConfig, RelationalOperatorConfig, HexColorConfig, IpConfig, MessageConfig, MaxDateConfig, NumberConfig, MaxTimeConfig, MinDateConfig, MinTimeConfig, NumericConfig, PasswordConfig, PatternConfig, RangeConfig, TimeConfig, BaseValidatorConfig, DateConfig } from '@nattyjs/validation-decorators'
 import { callValidator } from '../functions/call-validators'
 export const Validators={
     required:(config?:RequiredConfig)=>callValidator(config,ControlValidators.required),
@@ -10,8 +10,8 @@ export const Validators={
     contains:(config?:DefaultConfig)=>callValidator(config,ControlValidators.contains),
     creditCard:(config?: CreditCardConfig)=>callValidator(config,ControlValidators.creditCard),
     cusip:(config?:BaseValidatorConfig)=>callValidator(config,ControlValidators.cusip),
-    dataUri:(config?:ArrayConfig)=>callValidator(config,ControlValidators.dataUri),
-    date:(config?:ArrayConfig)=>callValidator(config,ControlValidators.date),
+    dataUri:(config?:BaseValidatorConfig)=>callValidator(config,ControlValidators.dataUri),
+    date:(config?:DateConfig)=>callValidator(config,ControlValidators.date),
     different:(config?:DifferentConfig)=>callValidator(config,ControlValidators.different),
     digit:(config?:DigitConfig)=>callValidator(config,ControlValidators.digit),
     email:(config?:EmailConfig)=>callValidator(config,ControlValidators.email),
