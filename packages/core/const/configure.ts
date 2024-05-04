0d259bf44b1c1d80416060d9b8be2de78dcf3647
Ajay Ojha <ojhaajay@outlook.com>
2024-05-04 07:28:08 +0530

    chore: change model name

--- packages/core/const/configure.ts
+++ packages/core/const/configure.ts
@@ -1,6 +1,6 @@
-import { NattyFormConfig } from "../interfaces/natty-form-config";
+import { FormConfig } from "../interfaces/natty-form-config";
 import { nattyForms } from "./natty-forms";
 
-export function configure(config: NattyFormConfig) {
+export function configure(config: FormConfig) {
     nattyForms.configure(config);
   }
--- packages/core/interfaces/natty-form-config.ts
+++ packages/core/interfaces/natty-form-config.ts
@@ -1,6 +1,6 @@
-import { FormConfig } from "@nattyjs/validation-decorators";
+import { FormConfig as NattyFormConfig } from "@nattyjs/validation-decorators";
 import { RunValidatorStrategy } from "../enums/run-validator-strategy";
 
-export interface NattyFormConfig extends FormConfig{
+export interface FormConfig extends NattyFormConfig{
     runValidatorStrategy?:RunValidatorStrategy;
 }
